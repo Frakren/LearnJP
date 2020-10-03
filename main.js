8b319f752667dc0966694f12b756c73ef7a666ad
Ericles Brum <35080587+Frakren@users.noreply.github.com>
2020-10-03 19:02:19 -0300

    Reestruturando

--- main.js
+++ main.js
@@ -1,41 +1,82 @@
-new Vue({
-    el:'#Menu',
+const hiragana=new Vue({
+    data:{
+        caracter:null
+    },
+    methods:{
+
+    },
+    template:`
+        <ul>
+            <li><a href="#" @click="">Alfabeto</a></li>
+            <li><a href="#" @click="">Adivinha</a></li>
+            <li><a href="#" @click="">Frases</a></li>
+        </ul>
+    `
+});
+const katakana=new Vue({
     data:{
 
     },
-    methods: {
-        loadContent(type){
-            switch(type)
-            {
-                case 0:
-                    alert("Hiragana");
-                    break;
-                case 1:
-                    alert("Katakana");
-                    break;
-                case 2:
-                    alert("Kanji");
-                    break;
-            };
-            new Vue({
-                el:'#task',
-                template:`
-                             <div>
-                                 <ul>
-                                     <li><a href="#" @click=""">Alfabeto</a></li>
-                                     <li><a href="#" @click=""">Adivinhe</a></li>
-                                     <li><a href="#" @click=""">FlashCard</a></li>
-                                 </ul>
-                             </div>
-                         `
-            }) 
-        }
+    methods:{
+
     },
     template:`
+        <ul>
+            <li><a href="#" @click="">Alfabeto</a></li>
+            <li><a href="#" @click="">Adivinha</a></li>
+            <li><a href="#" @click="">Frases</a></li>
+        </ul>
+    `
+});
+const kanji=new Vue({
+    data:{
+
+    },
+    methods:{
+
+    },
+    template:`
+        <ul>
+            <li><a href="#" @click="">Alfabeto</a></li>
+            <li><a href="#" @click="">Adivinha</a></li>
+            <li><a href="#" @click="">Frases</a></li>
+        </ul>
+    `
+});
+Vue.component('bmenu',{
+    data: function() {
+        return {
+        }
+    },
+    methods:{
+        loadContent(type){ 
+            switch(type){
+                case 0:
+                    hiragana.$mount('#b_TypeTask');
+                    $.getJSON("../hiragana.json", function(json) {
+                        hiragana.caracter=json;
+                        console.log(hiragana.caracter);
+                    });                
+                    break;
+                case 1:
+                    katakana.$mount('#b_TypeTask');
+                    break;
+                case 2:
+                    kanji.$mount('#b_TypeTask');
+                    break;
+                default:
+            }
+        }
+    },
+    template : `
     <ul>
         <li><a href="#" @click="loadContent(0)">Hiragana</a></li>
         <li><a href="#" @click="loadContent(1)">Katakana</a></li>
         <li><a href="#" @click="loadContent(2)">Kanji</a></li>
     </ul>
     `
-})
+});
+
+new Vue({
+    el:'#b_menu'
+});
